2773c6f5437b3d096ead0226198c2d2d7af35519
salah <salah.m.abotaleb@gmail.com>
2024-03-09 13:18:57 +0200

    Login page done Added: -Login buttons        -Password & Username fields

--- lib/features/Authentication/login.dart
+++ lib/features/Authentication/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:reddit_clone/common/FullWidthButton.dart';
 import 'package:reddit_clone/constants/assets_constants.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:reddit_clone/features/Authentication/widgets/auth_filed.dart';
 import 'package:reddit_clone/theme/pallete.dart';
 import 'package:reddit_clone/common/ImageButton.dart';
 
@@ -25,25 +26,50 @@ class LoginScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold))),
         ],
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 20),
-          Text('Log in to Reddit',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-          SizedBox(height: 20),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                ImageButton(
-                  text: 'Continue with Google',
-                  onPressed: () {},
-                  iconPath: AssetsConstants.googleLogo,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Text('Log in to Reddit',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 20),
+            ImageButton(
+              text: 'Continue with Google',
+              onPressed: () {},
+              iconPath: AssetsConstants.googleLogo,
+            ),
+            const Row(
+              children: <Widget>[
+                Expanded(
+                  child: Divider(
+                    color: Palette.redditGrey,
+                    height: 50,
+                  ),
+                ),
+                Text('   OR   ',
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Palette.redditDarkGrey)),
+                Expanded(
+                  child: Divider(
+                    color: Palette.redditGrey,
+                    height: 50,
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            AuthField(
+                controller: TextEditingController(), labelText: 'Username'),
+            const SizedBox(height: 20),
+            AuthField(
+                controller: TextEditingController(),
+                labelText: 'Password',
+                obscureText: true),
+          ],
+        ),
       ),
       persistentFooterButtons: [
         Padding(
