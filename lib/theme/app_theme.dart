24d4de1915ae153b669e7e37e7a7cecb2641f035
Osama-Moh <usama.nasser21@gmail.com>
2024-03-11 19:37:41 +0200

    begin theme

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/theme/pallete.dart';
+
+class AppTheme {
+  const AppTheme();
+
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: Pallete.blackColor,
+
+  );
+}
---
+++ lib/theme/pallete.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pallete{
+  const Pallete();
+
+  static const Color primaryColor = Color(0xFFFF5700);
+
+  static const Color secondaryColor = Color(0xFFFF4500);
+
+  static const Color whiteColor = Color(0xFFFFFFFF);
+
+  static const Color greyColor = Color(0xFFCCCCCC);
+
+  static const Color blackColor = Color(0xFF000000);
+}
