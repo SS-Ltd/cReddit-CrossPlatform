4b936095351c16df548a0f76eb721be7bf3c68f9
Osama-Moh <usama.nasser21@gmail.com>
2024-03-24 20:27:07 +0200

    solve warnings

--- lib/report_button.dart
+++ lib/report_button.dart
@@ -4,8 +4,12 @@ import 'package:http/testing.dart';
 import 'dart:convert';
 
 class ReportButton extends StatefulWidget {
+  const ReportButton({super.key});
+
   @override
-  _ReportButtonState createState() => _ReportButtonState();
+  State<ReportButton> createState() {
+    return _ReportButtonState();
+  }
 }
 
 class _ReportButtonState extends State<ReportButton> {
@@ -40,13 +44,13 @@ class _ReportButtonState extends State<ReportButton> {
         return StatefulBuilder(
           builder: (BuildContext context, StateSetter setModalState) {
             return Container(
-              color: Color.fromRGBO(34, 34, 34, 1),
+              color: const Color.fromRGBO(34, 34, 34, 1),
               padding:
                   EdgeInsets.only(bottom: MediaQuery.of(bc).viewInsets.bottom),
               child: Wrap(
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
+                  const Padding(
+                    padding: EdgeInsets.all(20.0),
                     child: Text(
                       'Submit a Report',
                       style: TextStyle(
@@ -55,8 +59,8 @@ class _ReportButtonState extends State<ReportButton> {
                           color: Colors.white),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
                     child: Text(
                       "Help fellow redditors by reporting things that break the rules. Let us know what's happening, and we'll look into it.",
                       style: TextStyle(color: Colors.white70),
@@ -89,7 +93,6 @@ class _ReportButtonState extends State<ReportButton> {
                       children: [
                         Expanded(
                           child: ElevatedButton(
-                            child: Text('Next', style: TextStyle(fontSize: 17)),
                             onPressed: () {
                               if (selectedReason != null) {
                                 reportPost('123', selectedReason!);
@@ -100,6 +103,8 @@ class _ReportButtonState extends State<ReportButton> {
                               foregroundColor: Colors.white,
                               backgroundColor: Colors.blue,
                             ),
+                            child: const Text('Next',
+                                style: TextStyle(fontSize: 17)),
                           ),
                         ),
                       ],
@@ -118,14 +123,14 @@ class _ReportButtonState extends State<ReportButton> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Report Button Page'),
-        backgroundColor: Color.fromRGBO(16, 16, 16, 1),
+        title: const Text('Report Button Page'),
+        backgroundColor: const Color.fromRGBO(16, 16, 16, 1),
       ),
       body: Center(
         child: ListTile(
-          tileColor: Color.fromRGBO(34, 34, 34, 1),
-          leading: Icon(Icons.flag, color: Colors.white),
-          title: Text(
+          tileColor: const Color.fromRGBO(34, 34, 34, 1),
+          leading: const Icon(Icons.flag, color: Colors.white),
+          title: const Text(
             'Report',
             style: TextStyle(color: Colors.white),
           ),
