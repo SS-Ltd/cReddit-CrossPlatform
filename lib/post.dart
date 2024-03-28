1cdbc27e1472a01fef6e227fc62a01a89b50d025
Tahoun99 <amkmtahoon@gmail.com>
2024-03-28 05:40:00 +0200

    Made post height in home page dynamic and made post_comment page but it is not fully finnished; there still two icons in app bar and integration of comments.

--- lib/post.dart
+++ lib/post.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/comments/comment_page.dart';
 import 'dart:async';
 import 'new_page.dart';
+import 'post_comment.dart';
+
 
 class Post extends StatefulWidget {
   final String communityName;
@@ -24,8 +26,8 @@ class Post extends StatefulWidget {
     this.shareNumber = 0,
     required this.timeStamp,
     this.isHomePage = true,
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<Post> createState() => _PostState();
@@ -37,214 +39,218 @@ class _PostState extends State<Post> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Card(
-              color: Colors.black,
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        const CircleAvatar(
-                          //replace with user profile picture
-                          backgroundImage: NetworkImage(
+    return Padding(
+      padding: const EdgeInsets.all(1.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Card(
+            color: Colors.black,
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        //replace with user profile picture
+                        backgroundImage: NetworkImage(
                             'https://www.w3schools.com/w3images/avatar2.png'),
-                        ),
-                        const SizedBox(width: 10),
-                        widget.isHomePage
-                        ? GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => NewPage()), //replace with community page
-                              );
-                            },
-                            child: Text(
-                              'r/${widget.communityName}',
-                              style: const TextStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                          )
-                        : Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
+                      ),
+                      const SizedBox(width: 10),
+                      widget.isHomePage
+                          ? GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const NewPage()), //replace with community page
+                                );
+                              },
+                              child: Text(
                                 'r/${widget.communityName}',
                                 style: const TextStyle(
-                                color: Colors.grey,
+                                  color: Colors.grey,
                                 ),
                               ),
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => NewPage()), //replace with profile page or widget
-                                  );
-                                },
-                                child: Text(
-                                  'u/${widget.userName} . ${formatTimestamp(widget.timeStamp)}',
+                            )
+                          : Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'r/${widget.communityName}',
                                   style: const TextStyle(
-                                    color: Colors.blue,
+                                    color: Colors.grey,
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                      ],
-                    ),
-                  ],
-                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const NewPage()), //replace with profile page or widget
+                                    );
+                                  },
+                                  child: Text(
+                                    'u/${widget.userName} . ${formatTimestamp(widget.timeStamp)}',
+                                    style: const TextStyle(
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 10),
-            GestureDetector(
-              onTap: widget.isHomePage
+          ),
+          // const SizedBox(height: 10),
+          GestureDetector(
+            onTap: widget.isHomePage
                 ? () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Post(
-                        communityName: widget.communityName,
-                        userName: widget.userName,
-                        title: widget.title,
-                        imageUrl: widget.imageUrl,
-                        content: widget.content,
-                        commentNumber: widget.commentNumber,
-                        shareNumber: widget.shareNumber,
-                        timeStamp: widget.timeStamp,
-                        isHomePage: false,
-                      )
-                    ), //replace with post and comment page
-                  );
-                }
-                : null,
-              child: Text(
-                widget.title,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Visibility(
-              visible: widget.imageUrl.isNotEmpty,  
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      // builder: (context) => ImageScreen(imageUrl: widget.imageUrl),
-                      builder: (context) => NewPage(),  //replace with image screen
-                    ),
-                  );
-                },
-                child: Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Image.network(
-                          widget.imageUrl,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                  ],
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: widget.isHomePage
-                ? () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Post(
-                        communityName: widget.communityName,
-                        userName: widget.userName,
-                        title: widget.title,
-                        imageUrl: widget.imageUrl,
-                        content: widget.content,
-                        commentNumber: widget.commentNumber,
-                        shareNumber: widget.shareNumber,
-                        timeStamp: widget.timeStamp,
-                        isHomePage: false,
-                      )
-                    ), //replace with post and comment page
-                  );
-                }
-                : null,
-              child: widget.isHomePage && widget.imageUrl.isEmpty
-                ? ( Text(
-                      widget.content,
-                      maxLines: 3,  
-                      overflow: TextOverflow.ellipsis,
-                    )
-                  )
-                : (!widget.isHomePage
-                  ? Text(widget.content)
-                  : const SizedBox.shrink()),  
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.arrow_upward),
-                  onPressed: () {
-                    setState(() {
-                      votes++;
-                    });
-                  },
-                ),
-                Text(votes.toString()),
-                IconButton(
-                  icon: const Icon(Icons.arrow_downward),
-                  onPressed: () {
-                    setState(() {
-                      votes--;
-                    });
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.add_comment), 
-                  //other icon: add_comment,comment
-                  onPressed: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const CommentPage(), 
-                      ),
+                        builder:(context) => PostComment(),
+                        // builder: (context) => Post(
+                        //   communityName: widget.communityName,
+                        //   userName: widget.userName,
+                        //   title: widget.title,
+                        //   imageUrl: widget.imageUrl,
+                        //   content: widget.content,
+                        //   commentNumber: widget.commentNumber,
+                        //   shareNumber: widget.shareNumber,
+                        //   timeStamp: widget.timeStamp,
+                        //   isHomePage: false,
+                        // ),
+                      ), //replace with post and comment page
                     );
-                  },
-                ),
-                Text(widget.commentNumber.toString()),
-                const Spacer(),
-                IconButton(
-                  icon: const Icon(Icons.ios_share),  
-                  //other icon: ios_share, share
-                  onPressed: () {
-                    // share post
-                  }, 
-                ),
-                if (widget.shareNumber > 0) Text(widget.shareNumber.toString()),
-              ],
+                  }
+                : null,
+            child: Text(
+              widget.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
             ),
-          ],
-        ),
+          ),
+          // const SizedBox(height: 10),
+          Visibility(
+            visible: widget.imageUrl.isNotEmpty,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    // builder: (context) => ImageScreen(imageUrl: widget.imageUrl),
+                    builder: (context) => const NewPage(),
+                    //replace with image screen
+                  ),
+                );
+              },
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.network(
+                        widget.imageUrl,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: widget.isHomePage
+                ? () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:(context) => PostComment(), 
+                        // builder: (context) => Post(
+                        //   communityName: widget.communityName,
+                        //   userName: widget.userName,
+                        //   title: widget.title,
+                        //   imageUrl: widget.imageUrl,
+                        //   content: widget.content,
+                        //   commentNumber: widget.commentNumber,
+                        //   shareNumber: widget.shareNumber,
+                        //   timeStamp: widget.timeStamp,
+                        //   isHomePage: false,
+                        // ),
+                      ), //replace with post and comment page
+                    );
+                  }
+                : null,
+            child: widget.isHomePage && widget.imageUrl.isEmpty
+                ? (Text(
+                    widget.content,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ))
+                : (!widget.isHomePage
+                    ? Text(widget.content)
+                    : const SizedBox.shrink()),
+          ),
+          // const SizedBox(height: 10),
+          Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.arrow_upward),
+                onPressed: () {
+                  setState(() {
+                    votes++;
+                  });
+                },
+              ),
+              Text(votes.toString()),
+              IconButton(
+                icon: const Icon(Icons.arrow_downward),
+                onPressed: () {
+                  setState(() {
+                    votes--;
+                  });
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.add_comment),
+                //other icon: add_comment,comment
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const CommentPage(),
+                    ),
+                  );
+                },
+              ),
+              Text(widget.commentNumber.toString()),
+              const Spacer(),
+              IconButton(
+                icon: const Icon(Icons.ios_share),
+                //other icon: ios_share, share
+                onPressed: () {
+                  // share post
+                },
+              ),
+              if (widget.shareNumber > 0) Text(widget.shareNumber.toString()),
+            ],
+          ),
+        ],
       ),
     );
   }
---
+++ lib/post_comment.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/post.dart';
+import 'package:reddit_clone/features/comments/comment_page.dart';
+
+
+class PostComment extends StatelessWidget {
+  PostComment({super.key});
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        key: _scaffoldKey,
+        // leading: const Icon(Icons.menu, size: 30.0),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search, size: 30.0),
+          ),
+          IconButton(
+            onPressed: () => _scaffoldKey.currentState!.openEndDrawer(),
+            icon: const Icon(Icons.reddit, size: 30.0),
+          ),
+        ],
+      ),
+      body: 
+        // ListView.builder(
+        // itemBuilder: (context, index) {
+        //   return mockPost();
+        // },
+        mockPost(),
+
+      // ),
+    );
+  }
+}
+
+Widget mockPost() {
+  return Column(
+    children: [
+      Post(
+        communityName: 'Entrepreneur',
+        userName: 'throwaway123',
+        title: 'Escaping corporate Hell and finding freedom',
+        imageUrl: 
+            'https://qph.cf2.quoracdn.net/main-qimg-e0b7b0c38b6cecad120db23705ccc4f3-pjlq',
+            // '',
+        content:
+            'Man, let me have a  vent for a minute. Just got out of the shittiest '
+            'gig ever – being a "marketing specialist" for the supposed big boys'
+            ' over at Microsoft. Let me tell you, it was not bad.',
+        commentNumber: 0,
+        shareNumber: 0,
+        timeStamp: DateTime.now(),
+        isHomePage: false,
+      ),
+      const Divider(height: 1, thickness: 1), // Add a thin horizontal line
+    ],
+  );
+}
+
