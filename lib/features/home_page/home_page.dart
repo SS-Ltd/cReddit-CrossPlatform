c40eae465add3149188bac3ea82777a53348bd14
Osama-Moh <usama.nasser21@gmail.com>
2024-03-31 16:59:53 +0200

    add file for dropdown list

--- lib/features/home_page/home_page.dart
+++ lib/features/home_page/home_page.dart
@@ -41,9 +41,9 @@ class HomePage extends StatelessWidget {
           communityName: 'Entrepreneur',
           userName: 'throwaway123',
           title: 'Escaping corporate Hell and finding freedom',
-          imageUrl: 
+          imageUrl:
               'https://qph.cf2.quoracdn.net/main-qimg-e0b7b0c38b6cecad120db23705ccc4f3-pjlq',
-              // '',
+          // '',
           content:
               'Man, let me have a  vent for a minute. Just got out of the shittiest '
               'gig ever – being a "marketing specialist" for the supposed big boys'
@@ -58,3 +58,16 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+// DropdownButton(
+//             value: shownvalue,
+//             icon: const Icon(Icons.arrow_downward),
+//             onChanged: (value) {
+//               setState(() {
+//                 shownvalue = value;
+//               });
+//             },
+//             items: list.map<DropdownMenuItem<String>>((String value) {
+//               return DropdownMenuItem<String>(value: value, child: Text(value));
+//             }).toList(),
+//           ),
