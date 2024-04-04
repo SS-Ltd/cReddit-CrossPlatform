0332ce21266aa59c71f559d1ccd3b2352db0dfcc
Osama-Moh <usama.nasser21@gmail.com>
2024-04-04 20:37:52 +0200

    add list of recentlyvisited in usermodel

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,10 +1,20 @@
 class UserModel {
   final String username;
+
   bool isLoggedIn = false;
+  final List<String> recentlyVisited = [];
 
   UserModel(this.username);
 
   void updateUserStatus(bool status) {
     isLoggedIn = status;
+  }
+
+  void setrecentlyVisited(String communityName) {
+    recentlyVisited.add(communityName);
+  }
+
+  List<String> getrecentlyvisited(){
+    return recentlyVisited;
   }
 }
