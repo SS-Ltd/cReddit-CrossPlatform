3fd188c19b214e75df85423a04e64df2b6495bc1
Osama-Moh <usama.nasser21@gmail.com>
2024-04-04 18:54:04 +0200

    add poll option

---
+++ lib/Network.dart
@@ -0,0 +1,237 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+// import 'package:reddit_clone/models/post_model.dart';
+// import 'package:reddit_clone/models/subreddit.dart';
+import 'dart:convert';
+
+// import 'package:reddit_clone/models/user.dart';
+// import 'package:reddit_clone/models/user_settings.dart';
+
+class NetworkService extends ChangeNotifier {
+  static final NetworkService _instance = NetworkService._internal();
+  factory NetworkService() => _instance;
+
+  NetworkService._internal();
+
+  String _baseUrl = 'http://10.0.2.2:3000';
+  String _cookie = '';
+  // UserModel? _user;
+  // UserModel? get user => _user;
+  // UserSettings? _userSettings;
+  // UserSettings? get userSettings => _userSettings;
+
+  // Future<void> login(String username, String password) async {
+  //   print('Logging in...');
+  //   Uri url = Uri.parse('$_baseUrl/user/login');
+  //   final response = await http.post(url,
+  //       headers: {'Content-Type': 'application/json'},
+  //       body: jsonEncode({'username': username, 'password': password}));
+
+  //   if (response.statusCode == 200) {
+  //     _updateCookie(response);
+  //     _user = UserModel(username);
+  //     _user!.updateUserStatus(true);
+  //     print('Logged in. Cookie: $_cookie');
+  //   } else {
+  //     print('Login failed: ${response.body}');
+  //   }
+  //   notifyListeners();
+  // }
+
+  Future<void> logout() async {
+    Uri url = Uri.parse('$_baseUrl/user/logout');
+    final response = await http.get(url, headers: _headers);
+    print('Logged out: ${response.body}');
+  }
+
+  // Future<void> getUserSettings() async {
+  //   Uri url = Uri.parse('$_baseUrl/user/settings');
+  //   final response = await http.get(url, headers: _headers);
+  //   print(response.body);
+  //   if (response.statusCode == 200) {
+  //     final Map<String, dynamic> json = jsonDecode(response.body);
+  //     _userSettings = UserSettings.fromJson(json);
+  //     notifyListeners(); // Notify listeners to update UI or other components listening to changes
+  //   } else {
+  //     print('Failed to fetch user settings: ${response.body}');
+  //   }
+  // }
+
+  Future<bool> forgotPassword(String username) async {
+    final url = Uri.parse('$_baseUrl/user/forgot-password');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'info': username}),
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> createUser(
+      String username, String email, String password, String gender) async {
+    final url = Uri.parse('$_baseUrl/user');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': username,
+        'email': email,
+        'password': password,
+        'gender': gender,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return true;
+    } else {
+      print('Failed to create user: ${response.body}');
+      return false;
+    }
+  }
+
+  Future<void> refreshToken() async {
+    Uri url = Uri.parse('$_baseUrl/user/refresh-token');
+
+    // Extract refreshToken from _headers['Cookie']
+    String? rawCookie = _headers['Cookie'];
+    String refreshToken = '';
+    if (rawCookie != null) {
+      refreshToken = rawCookie.split(',').firstWhere(
+          (element) => element.contains('refreshToken'),
+          orElse: () => '');
+    }
+
+    final response = await http.get(url, headers: {'Cookie': refreshToken});
+    print("alo?");
+    print(_headers['Cookie']);
+    if (response.statusCode == 200) {
+      _updateCookie(response);
+      print('Token refreshed successfully. New Cookie: $_cookie');
+    } else {
+      print('Failed to refresh token: ${response.body}');
+    }
+  }
+
+  Future<bool> createCommunity(String name, bool isNSFW) async {
+    Uri url = Uri.parse('$_baseUrl/subreddit');
+    final response = await http.post(
+      url,
+      headers: _headers,
+      body: jsonEncode({
+        'name': name,
+        'isNSFW': isNSFW,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return true;
+    } else {
+      print('Failed to create community: ${response.body}');
+      return false;
+    }
+  }
+
+  Future <bool> createNewPost(String name) async {
+    Uri url = Uri.parse('$_baseUrl/post');
+    final response = await http.post(
+      url,
+      body: jsonEncode({
+        
+      }),    
+    );
+    if(response.statusCode == 200)
+    {
+      return true;
+    }
+    else
+    {
+      print('Failed to create post: ${response.body}');
+      return false;
+    }
+  }
+
+  Future<bool> isSubredditNameAvailable(String name) async {
+    Uri url = Uri.parse('$_baseUrl/subreddit/is-name-available/$name');
+    final response = await http.get(url, headers: _headers);
+
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      return body['available'] ?? false;
+    } else {
+      // Handle error or assume name is not available
+      return false;
+    }
+  }
+
+  // Future<Subreddit?> getSubredditDetails(String subredditName) async {
+  //   Uri url = Uri.parse('$_baseUrl/subreddit/$subredditName');
+  //   final response = await http.get(url, headers: _headers);
+
+  //   if (response.statusCode == 200) {
+  //     final json = jsonDecode(response.body);
+  //     return Subreddit.fromJson(json);
+  //   } else {
+  //     // Failed to fetch details or handle specific error
+  //     return null;
+  //   }
+  // }
+
+  // Future<List<PostModel>?> fetchPostsForSubreddit(String subredditName) async {
+  //   Uri url = Uri.parse('$_baseUrl/subreddit/$subredditName/posts');
+  //   final response = await http.get(url, headers: _headers);
+  //   print(response.body);
+  //   if (response.statusCode == 200) {
+  //     final List<dynamic> responseData = json.decode(response.body);
+  //     return responseData
+  //         .map((postJson) => PostModel.fromJson(postJson))
+  //         .toList();
+  //   } else {
+  //     return null;
+  //   }
+  // }
+
+  // Future<List<PostModel>?> getSavedPosts() async {
+  //   final url = Uri.parse('$_baseUrl/user/saved');
+  //   final response = await http.get(url, headers: _headers);
+  //   print(response.body);
+  //   if (response.statusCode == 200) {
+  //     List<dynamic> jsonData = json.decode(response.body);
+  //     List<PostModel> posts =
+  //         jsonData.map((item) => PostModel.fromJson(item)).toList();
+  //     return posts;
+  //   } else {
+  //     return null;
+  //   }
+  // }
+
+  // Future<List<PostModel>?> getUserHistory() async {
+  //   final url = Uri.parse('$_baseUrl/user/history');
+  //   final response = await http.get(url, headers: _headers);
+  //   print(response.body);
+  //   if (response.statusCode == 200) {
+  //     List<dynamic> jsonData = json.decode(response.body);
+  //     List<PostModel> posts =
+  //         jsonData.map((item) => PostModel.fromJson(item)).toList();
+  //     return posts;
+  //   } else {
+  //     return null;
+  //   }
+  // }
+
+  void _updateCookie(http.Response response) {
+    String? rawCookie = response.headers['set-cookie'];
+    if (rawCookie != null) {
+      _cookie = rawCookie;
+    }
+  }
+
+  Map<String, String> get _headers => {
+        'Content-Type': 'application/json',
+        if (_cookie.isNotEmpty) 'Cookie': _cookie,
+      };
+}
--- lib/create_post.dart
+++ lib/create_post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:reddit_clone/community_choice.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
@@ -23,13 +24,14 @@ class CreatePost extends StatefulWidget {
 }
 
 class _CreatePostState extends State<CreatePost> {
-
   final _formKey = GlobalKey<FormState>();
   final _titleController = TextEditingController();
   final _bodyController = TextEditingController();
+
   bool _istitleempty = true;
   bool _isbodyempty = true;
   String chosenCommunity = "";
+
   File? _image;
   final ImagePicker picker = ImagePicker();
   bool _isImagePickerOpen = false;
@@ -37,6 +39,9 @@ class _CreatePostState extends State<CreatePost> {
   final _linkController = TextEditingController();
   bool _insertlink = false;
   bool _islinkempty = true;
+
+  bool _insertpoll = false;
+  int count = 0;
 
   Future getImage() async {
     final image = await picker.pickImage(source: ImageSource.gallery);
@@ -53,8 +58,7 @@ class _CreatePostState extends State<CreatePost> {
     super.dispose();
   }
 
-  Future <void> createNewPost () async {
-  }
+  Future<void> createNewPost() async {}
 
   @override
   Widget build(BuildContext context) {
@@ -117,23 +121,30 @@ class _CreatePostState extends State<CreatePost> {
                   });
                 },
               ),
-              _insertlink ? TextField(
-                decoration: InputDecoration(
-                  labelText: _islinkempty ? 'Enter link' : '',
-                  labelStyle: const TextStyle(fontSize: 30),
-                  border: InputBorder.none,
-                  suffixIcon: IconButton(onPressed: () {setState(() {
-                    _linkController.clear();
-                    _insertlink = false;
-                  });}, icon: const Icon(Icons.close),),
-                ),
-                controller: _linkController,
-                onChanged: (value) {
-                  setState(() {
-                    _islinkempty = value.isEmpty;
-                  });
-                },
-              ) : const SizedBox(),
+              _insertlink
+                  ? TextField(
+                      decoration: InputDecoration(
+                        labelText: _islinkempty ? 'Enter link' : '',
+                        labelStyle: const TextStyle(fontSize: 30),
+                        border: InputBorder.none,
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _linkController.clear();
+                              _insertlink = false;
+                            });
+                          },
+                          icon: const Icon(Icons.close),
+                        ),
+                      ),
+                      controller: _linkController,
+                      onChanged: (value) {
+                        setState(() {
+                          _islinkempty = value.isEmpty;
+                        });
+                      },
+                    )
+                  : const SizedBox(),
               TextField(
                 decoration: InputDecoration(
                   labelText: _isbodyempty ? 'body text (optional)' : '',
@@ -167,13 +178,106 @@ class _CreatePostState extends State<CreatePost> {
                     icon: const Icon(Icons.video_library_outlined),
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        _insertpoll = !_insertpoll;
+                      });
+                    },
                     icon: const Icon(Icons.poll_outlined),
                   ),
                 ],
               ),
+              _insertpoll ? openpollcreator() : const SizedBox(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget openpollcreator() {
+    final _option1 = TextEditingController();
+    final _option2 = TextEditingController();
+
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Poll ends in'),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _insertpoll = !_insertpoll;
+                    });
+                  },
+                  icon: const Icon(Icons.close),
+                ),
+              ],
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Option 1',
+                border: InputBorder.none,
+              ),
+              controller: _option1,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Option 2',
+                border: InputBorder.none,
+              ),
+              controller: _option2,
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: count,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Option ${index + 3}',
+                        ),
+                        onChanged: (value) {
+                          // Handle the input value for each option
+                        },
+                      ),
+                    ),
+                    IconButton(onPressed: () {
+                      setState(() {
+                        count = count - 1;
+                      });
+                    }, icon: const Icon(Icons.close))
+                  ],
+                );
+              },
+            ),
+            (count < 4)
+                ? Row(
+                    children: [
+                      TextButton.icon(
+                        onPressed: () {
+                          setState(() {
+                            count = count + 1;
+                            print(count);
+                          });
+                        },
+                        label: const Text('Add option'),
+                        icon: const Icon(Icons.add),
+                      )
+                    ],
+                  )
+                : const SizedBox(),
+          ],
         ),
       ),
     );
