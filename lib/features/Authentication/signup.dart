ef00ee1f72c07909b44e1c4787de6ce966449840
salah <salah.m.abotaleb@gmail.com>
2024-04-09 18:05:31 +0200

    Fix signup/signin buttons

--- lib/features/Authentication/signup.dart
+++ lib/features/Authentication/signup.dart
@@ -82,42 +82,46 @@ class SignUpScreen extends StatelessWidget {
             ),
             if (!isKeyboardOpen) const AgreementText(),
             const SizedBox(height: 20),
-            FullWidthButton(
-              text: "Continue",
-              onPressed: () async {
-                if (emailController.text.isEmpty ||
-                    passwordController.text.isEmpty) {
-                  // Show an error message
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Please fill in all fields')),
-                  );
-                  return;
-                }
-                if (!isValidEmail(emailController.text)) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Invalid email address')),
-                  );
-                  return;
-                }
-                if (!isValidPassword(passwordController.text)) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                        content: Text(
-                            'Password must be 8 or more characters and contain at least one uppercase and one lowercase letter')),
-                  );
-                  return;
-                }
-                Map<String, dynamic> userData = {
-                  'email': emailController.text,
-                  'password': passwordController.text,
-                };
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => NameSuggestion(userData: userData)),
-                );
-              },
-            ),
+            Visibility(
+                visible: !isKeyboardOpen,
+                child: FullWidthButton(
+                  text: "Continue",
+                  onPressed: () async {
+                    if (emailController.text.isEmpty ||
+                        passwordController.text.isEmpty) {
+                      // Show an error message
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('Please fill in all fields')),
+                      );
+                      return;
+                    }
+                    if (!isValidEmail(emailController.text)) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Invalid email address')),
+                      );
+                      return;
+                    }
+                    if (!isValidPassword(passwordController.text)) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text(
+                                'Password must be 8 or more characters and contain at least one uppercase and one lowercase letter')),
+                      );
+                      return;
+                    }
+                    Map<String, dynamic> userData = {
+                      'email': emailController.text,
+                      'password': passwordController.text,
+                    };
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              NameSuggestion(userData: userData)),
+                    );
+                  },
+                )),
           ],
         ),
       ),
