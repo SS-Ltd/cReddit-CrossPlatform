502ff903900e1c6ca5e706a108ebb3ff114b4194
Tahoun99 <amkmtahoon@gmail.com>
2024-04-11 19:50:40 +0200

    profile GUI is almost done

--- lib/features/User/Profile.dart
+++ lib/features/User/Profile.dart
@@ -440,40 +440,40 @@ List<PopupMenuEntry<Menu>> menuitems() {
 }
 
 
-class DropdownButton extends StatefulWidget {
-  const DropdownButton({super.key, required String value, required Icon icon});
+// class DropdownButton extends StatefulWidget {
+//   const DropdownButton({super.key, required String value, required Icon icon});
 
-  @override
-  State<DropdownButton> createState() => _DropdownButtonState();
-}
+//   @override
+//   State<DropdownButton> createState() => _DropdownButtonState();
+// }
 
-const List<String> list = <String>['top', 'hot', 'new'];
-class _DropdownButtonState extends State<DropdownButton> {
-  String dropdownValue = list.first;
+// const List<String> list = <String>['top', 'hot', 'new'];
+// class _DropdownButtonState extends State<DropdownButton> {
+//   String dropdownValue = list.first;
 
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      items: list.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return DropdownButton(
+//       value: dropdownValue,
+//       icon: const Icon(Icons.arrow_downward),
+//       elevation: 16,
+//       style: const TextStyle(color: Colors.deepPurple),
+//       underline: Container(
+//         height: 2,
+//         color: Colors.deepPurpleAccent,
+//       ),
+//       onChanged: (String? value) {
+//         // This is called when the user selects an item.
+//         setState(() {
+//           dropdownValue = value!;
+//         });
+//       },
+//       items: list.map<DropdownMenuItem<String>>((String value) {
+//         return DropdownMenuItem<String>(
+//           value: value,
+//           child: Text(value),
+//         );
+//       }).toList(),
+//     );
+//   }
+// }
