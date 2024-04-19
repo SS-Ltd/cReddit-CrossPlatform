66652e9d1894619ca8ec804c0dfa3428368b0ceb
YoussefDarwishh <youssef.darwish03@eng-st.cu.edu.eg>
2024-04-19 16:18:41 +0200

    fix: redirect to subreddit in top communities

--- lib/features/community/community_page.dart
+++ lib/features/community/community_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/features/community/subreddit_page.dart';
 import 'package:reddit_clone/models/community.dart';
 import 'community_card.dart';
 import 'package:provider/provider.dart';
@@ -8,7 +9,6 @@ import 'package:reddit_clone/theme/palette.dart';
 /// This class represents the community page in the application.
 /// It is a stateful widget that fetches a list of communities and displays them in a ListView.
 class CommunityPage extends StatefulWidget {
-
   const CommunityPage({super.key});
 
   @override
@@ -49,8 +49,20 @@ class CommunityPageState extends State<CommunityPage> {
             body: ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (BuildContext context, int index) {
-                return CommunityCard(
-                  community: snapshot.data![index],
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SubRedditPage(
+                          subredditName: snapshot.data![index].name,
+                        ),
+                      ),
+                    );
+                  },
+                  child: CommunityCard(
+                    community: snapshot.data![index],
+                  ),
                 );
               },
             ),
