d2293296dbd409da799cf5586b209691eccad950
YoussefDarwishh <youssef.darwish03@eng-st.cu.edu.eg>
2024-04-23 21:14:39 +0200

    Update chat_screen.dart

--- lib/features/chat/chat_screen.dart
+++ lib/features/chat/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/models/chatmessages.dart';
+import 'package:reddit_clone/new_page.dart';
 
 class ChatScreen extends StatefulWidget {
   final String recipientId;
@@ -11,18 +12,35 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  List<ChatMessage> messages =
-      []; // This will be replaced with your actual messages
+  List<ChatMessage> messages = [];
   final messageController = TextEditingController();
+  bool isFirstMessage = true; // Assuming this is to check if it's a new chat
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat with ${widget.recipientId}'),
+        title: Row(
+          children: <Widget>[
+            const Expanded(
+              child: Text(
+                'Username', // This should be the recipient's username
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const NewPage()));
+              },
+            ),
+          ],
+        ),
       ),
       body: Column(
         children: [
+          isFirstMessage ? introSection() : Container(),
           Expanded(
             child: ListView.builder(
               itemCount: messages.length,
@@ -36,27 +54,85 @@ class _ChatScreenState extends State<ChatScreen> {
               },
             ),
           ),
-          TextField(
-            controller: messageController,
-            decoration: InputDecoration(
-              labelText: 'Type a message',
-              suffixIcon: IconButton(
-                icon: Icon(Icons.send),
-                onPressed: () {
-                  final message = ChatMessage(
-                    senderId:
-                        'currentUserId', // Replace with the current user's ID
-                    recipientId: widget.recipientId,
-                    message: messageController.text,
-                    timestamp: DateTime.now(),
-                  );
-                  setState(() {
-                    messages.add(message);
-                  });
-                  messageController.clear();
-                },
+          messageInputField(),
+        ],
+      ),
+    );
+  }
+
+  Widget introSection() {
+    return Column(
+      children: [
+        SizedBox(height: 20),
+        const CircleAvatar(
+          radius: 60,
+          backgroundImage: NetworkImage(
+              "https://i.imgur.com/BoN9kdC.png"), // Recipient's large profile picture
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Username', // Recipient's username
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Text('Redditor for 7m 25d'),
+        TextButton(
+          onPressed: () {}, // Navigate to recipient's profile
+          child: Text('View Profile'),
+        ),
+      ],
+    );
+  }
+
+  Widget messageInputField() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.camera_alt),
+            onPressed: () {
+              // Logic to open camera
+            },
+          ),
+          Expanded(
+            child: TextField(
+              controller: messageController,
+              decoration: InputDecoration(
+                hintText: 'Type a message',
+                border: InputBorder.none,
               ),
+              maxLines: null, // Allows input field to expand as text wraps
             ),
+          ),
+          IconButton(
+            icon: Icon(Icons.gif),
+            onPressed: () {
+              // Logic to send GIF
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.emoji_emotions),
+            onPressed: () {
+              // Logic to insert emoji
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            color: Colors.blue,
+            onPressed: () {
+              final message = ChatMessage(
+                senderId: 'currentUserId', // Replace with the current user's ID
+                recipientId: widget.recipientId,
+                message: messageController.text,
+                timestamp: DateTime.now(),
+              );
+              setState(() {
+                messages.add(message);
+                isFirstMessage =
+                    false; // After the first message, remove the intro section
+              });
+              messageController.clear();
+            },
           ),
         ],
       ),
