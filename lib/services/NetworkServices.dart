85b1f935451fe186d72eb51699cd9bae4609703a
YoussefDarwishh <youssef.darwish03@eng-st.cu.edu.eg>
2024-04-30 20:31:25 +0300

    fixed profile picture issue in notifications

--- lib/services/NetworkServices.dart
+++ lib/services/NetworkServices.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:reddit_clone/models/messages.dart';
@@ -450,7 +452,8 @@ class NetworkService extends ChangeNotifier {
     }
   }
 
-  Future<List<Messages>?> fetchInboxMessages({int page = 1, int limit = 10}) async {
+  Future<List<Messages>?> fetchInboxMessages(
+      {int page = 1, int limit = 10}) async {
     Uri url = Uri.parse('$_baseUrl/message/?page=$page&limit=$limit');
     final response = await http.get(url, headers: _headers);
     if (response.statusCode == 403) {
