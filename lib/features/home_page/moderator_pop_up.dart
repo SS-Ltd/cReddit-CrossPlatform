f2b8bdac55659780393e61ef26aec7a22b34dbc9
Tahoun99 <amkmtahoon@gmail.com>
2024-04-30 15:07:33 +0300

    the moderator pop up is in the post
    implemented the moderator pop up is in the post, still need the end points for the buttons to make an action.

--- lib/features/home_page/moderator_pop_up.dart
+++ lib/features/home_page/moderator_pop_up.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:reddit_clone/common/arrow_button.dart';
+import 'package:reddit_clone/models/post_model.dart';
+import 'package:reddit_clone/theme/theme.dart';
 
 class ModeratorPopUP extends StatelessWidget {
-  const ModeratorPopUP({super.key});
+  final PostModel postModel;
+  const ModeratorPopUP({
+    required this.postModel, 
+    super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,43 +16,54 @@ class ModeratorPopUP extends StatelessWidget {
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(0)),
       ),
-      content: Column(
+      content: SingleChildScrollView(
+      child: Column( 
         children: [
           ArrowButton(
                 onPressed: () {}, // Replace print('Moderator') with an empty function.
               buttonText: "Approve post",
-              buttonIcon: Icons.check),
+              buttonIcon: Icons.check,
+              hasarrow: false
+              ),
           ArrowButton(
               onPressed: () {},
               buttonText: "Remove post",
-              buttonIcon: Icons.remove_circle_outline),
+              buttonIcon: Icons.remove_circle_outline,
+              hasarrow: false),
           ArrowButton(
               onPressed: () {},
               buttonText: "Remove as spam",
-              buttonIcon: Icons.remove_from_queue_outlined),
+              buttonIcon: Icons.remove_from_queue_outlined,
+              hasarrow: false),
           ArrowButton(
               onPressed: () {},
               buttonText: "Lock comments",
-              buttonIcon: Icons.lock_outline),
+              buttonIcon: Icons.lock_outline,
+              hasarrow: false),
           ArrowButton(
             onPressed: () {}, 
             buttonText: "Sticky post", 
-            buttonIcon: Icons.push_pin_outlined),
+            buttonIcon: Icons.push_pin_outlined,
+            hasarrow: false),
           ArrowButton(
               onPressed: () {},
               buttonText: "Mark as spoiler",
-              buttonIcon: Icons.warning_amber_outlined),
+              buttonIcon: Icons.warning_amber_outlined,
+              hasarrow: false),
           ArrowButton( 
               onPressed: () {},
               buttonText: "Mark as NSFW",
-              buttonIcon: Icons.eighteen_up_rating_outlined),
+              buttonIcon: Icons.eighteen_up_rating_outlined,
+              hasarrow: false),
           ArrowButton(
             onPressed: () {}, 
             buttonText: "Adjust Croud Control", 
-            buttonIcon: Icons.group_outlined),
+            buttonIcon: Icons.group_outlined,
+            hasarrow: false),
           
         ],
       ),
+    ),
     );
   }
 }
