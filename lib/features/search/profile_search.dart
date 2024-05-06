197a2c9cd2fcec5103ef5061ddd0468ca94f0889
Osama-Moh <usama.nasser21@gmail.com>
2024-05-06 14:24:56 +0300

    adjust search

--- lib/features/search/profile_search.dart
+++ lib/features/search/profile_search.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/features/comments/comment_page.dart';
+import 'package:reddit_clone/features/home_page/post.dart';
 import 'package:reddit_clone/features/search/post_tile.dart';
 import 'package:reddit_clone/models/search.dart';
 import 'package:provider/provider.dart';
@@ -27,6 +29,8 @@ class _ProfileSearchState extends State<ProfileSearch>
   int _selectedIndex = 0;
   String searchQuery = '';
   final FocusNode _focusNode = FocusNode();
+  String sortOption = "Most relevant";
+  String timeOption = "All time";
 
   @override
   void initState() {
@@ -64,10 +68,10 @@ class _ProfileSearchState extends State<ProfileSearch>
           onSubmitted: (value) async {
             commentsResults =
                 await Provider.of<NetworkService>(context, listen: false)
-                    .getSearchComments(value, widget.username);
-            postsResults =
-                await Provider.of<NetworkService>(context, listen: false)
-                    .getSearchPosts(value, widget.username);
+                    .getSearchComments(value, widget.username, sortOption);
+            postsResults = await Provider.of<NetworkService>(context,
+                    listen: false)
+                .getSearchPosts(value, widget.username, sortOption, timeOption);
             setState(() {
               isSearching = false;
               searchQuery = value;
@@ -117,13 +121,82 @@ class _ProfileSearchState extends State<ProfileSearch>
                             showModalBottomSheet(
                               context: context,
                               builder: (BuildContext context) {
-                                return BottomSheet(
-                                  onClosing: () {},
-                                  builder: (BuildContext context) {
-                                    return const Column(
-                                      children: [],
-                                    );
-                                  },
+                                return Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    BottomSheet(
+                                      onClosing: () {},
+                                      builder: (BuildContext context) {
+                                        return Column(
+                                          children: [
+                                            const Text(
+                                              "Sort by",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 18),
+                                            ),
+                                            RadioListTile(
+                                              title:
+                                                  const Text("Most relevant"),
+                                              value: 'Most relevant',
+                                              groupValue: sortOption,
+                                              onChanged: (value) {
+                                                setState(
+                                                  () {
+                                                    sortOption =
+                                                        value.toString();
+                                                  },
+                                                );
+                                                Navigator.pop(context);
+                                              },
+                                            ),
+                                            RadioListTile(
+                                              title: const Text("Hot"),
+                                              value: 'Hot',
+                                              groupValue: sortOption,
+                                              onChanged: (value) {
+                                                setState(
+                                                  () {
+                                                    sortOption =
+                                                        value.toString();
+                                                  },
+                                                );
+                                                Navigator.pop(context);
+                                              },
+                                            ),
+                                            RadioListTile(
+                                              title: const Text("New"),
+                                              value: 'New',
+                                              groupValue: sortOption,
+                                              onChanged: (value) {
+                                                setState(
+                                                  () {
+                                                    sortOption =
+                                                        value.toString();
+                                                  },
+                                                );
+                                                Navigator.pop(context);
+                                              },
+                                            ),
+                                            RadioListTile(
+                                              title: const Text("Top"),
+                                              value: 'Top',
+                                              groupValue: sortOption,
+                                              onChanged: (value) {
+                                                setState(
+                                                  () {
+                                                    sortOption =
+                                                        value.toString();
+                                                  },
+                                                );
+                                                Navigator.pop(context);
+                                              },
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    ),
+                                  ],
                                 );
                               },
                             );
@@ -139,7 +212,124 @@ class _ProfileSearchState extends State<ProfileSearch>
                         height: 35,
                         child: _selectedIndex == 0
                             ? ElevatedButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  showModalBottomSheet(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          BottomSheet(
+                                            onClosing: () {},
+                                            builder: (BuildContext context) {
+                                              return Column(
+                                                children: [
+                                                  const Text(
+                                                    "Filter by time",
+                                                    style: TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                        fontSize: 18),
+                                                  ),
+                                                  RadioListTile(
+                                                    title:
+                                                        const Text("All time"),
+                                                    value: 'All time',
+                                                    groupValue: timeOption,
+                                                    onChanged: (value) {
+                                                      setState(
+                                                        () {
+                                                          timeOption =
+                                                              value.toString();
+                                                        },
+                                                      );
+                                                      Navigator.pop(context);
+                                                    },
+                                                  ),
+                                                  RadioListTile(
+                                                    title:
+                                                        const Text("Past year"),
+                                                    value: 'Past year',
+                                                    groupValue: timeOption,
+                                                    onChanged: (value) {
+                                                      setState(
+                                                        () {
+                                                          timeOption =
+                                                              value.toString();
+                                                        },
+                                                      );
+                                                      Navigator.pop(context);
+                                                    },
+                                                  ),
+                                                  RadioListTile(
+                                                    title: const Text(
+                                                        "Past month"),
+                                                    value: 'Past month',
+                                                    groupValue: timeOption,
+                                                    onChanged: (value) {
+                                                      setState(
+                                                        () {
+                                                          timeOption =
+                                                              value.toString();
+                                                        },
+                                                      );
+                                                      Navigator.pop(context);
+                                                    },
+                                                  ),
+                                                  RadioListTile(
+                                                    title:
+                                                        const Text("Past week"),
+                                                    value: 'Past week',
+                                                    groupValue: timeOption,
+                                                    onChanged: (value) {
+                                                      setState(
+                                                        () {
+                                                          timeOption =
+                                                              value.toString();
+                                                        },
+                                                      );
+                                                      Navigator.pop(context);
+                                                    },
+                                                  ),
+                                                  RadioListTile(
+                                                    title: const Text(
+                                                        "Past 24 hours"),
+                                                    value: 'Past 24 hours',
+                                                    groupValue: timeOption,
+                                                    onChanged: (value) {
+                                                      setState(
+                                                        () {
+                                                          timeOption =
+                                                              value.toString();
+                                                        },
+                                                      );
+                                                      Navigator.pop(context);
+                                                    },
+                                                  ),
+                                                  RadioListTile(
+                                                    title:
+                                                        const Text("Past hour"),
+                                                    value: 'Past hour',
+                                                    groupValue: timeOption,
+                                                    onChanged: (value) {
+                                                      setState(
+                                                        () {
+                                                          timeOption =
+                                                              value.toString();
+                                                        },
+                                                      );
+                                                      Navigator.pop(context);
+                                                    },
+                                                  ),
+                                                ],
+                                              );
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
                                 child: const Text("Time"),
                               )
                             : null,
@@ -156,10 +346,52 @@ class _ProfileSearchState extends State<ProfileSearch>
                           : ListView.builder(
                               itemCount: postsResults.length,
                               itemBuilder: (context, index) {
+                                return GestureDetector(
+                                  onTap: () async {
+                                    Post postComment = Post(
+                                      postModel: await Provider.of<
+                                                  NetworkService>(context,
+                                              listen: false)
+                                          .fetchPost(postsResults[index].id),
+                                      isHomePage: false,
+                                      isSubRedditPage: false,
+                                    );
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => CommentPage(
+                                          postId: postsResults[index].id,
+                                          postComment: postComment,
+                                          postTitle: postsResults[index].title,
+                                          username:
+                                              postsResults[index].username,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  child: Column(
+                                    children: [
+                                      PostTile(
+                                          post: postsResults[index],
+                                          isProfile: true),
+                                      const Divider(
+                                        thickness: 1,
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                      //Comments
+                      commentsResults.isEmpty
+                          ? noResults()
+                          : ListView.builder(
+                              itemCount: commentsResults.length,
+                              itemBuilder: (context, index) {
                                 return Column(
                                   children: [
-                                    PostTile(
-                                        post: postsResults[index],
+                                    CommentTile(
+                                        comment: commentsResults[index],
                                         isProfile: true),
                                     const Divider(
                                       thickness: 1,
@@ -168,22 +400,6 @@ class _ProfileSearchState extends State<ProfileSearch>
                                 );
                               },
                             ),
-                      //Comments
-                      commentsResults.isEmpty
-                          ? noResults()
-                          : ListView.builder(
-                          itemCount: commentsResults.length,
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                CommentTile(comment: commentsResults[index], isProfile: true),
-                                const Divider(
-                                  thickness: 1,
-                                ),
-                              ],
-                            );
-                          },
-                        ), 
                     ],
                   ),
                 ),
