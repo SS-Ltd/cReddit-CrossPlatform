b60e71706b2080dc6203c7f4f3b8990d22ab69ff
Osama-Moh <usama.nasser21@gmail.com>
2024-05-07 17:22:21 +0300

    manage notifications

--- lib/features/settings/manage_notifications.dart
+++ lib/features/settings/manage_notifications.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/common/heading.dart';
 import 'package:reddit_clone/common/switch_button.dart';
+import 'package:reddit_clone/features/settings/settings.dart';
 import 'package:reddit_clone/services/networkServices.dart';
 import 'package:provider/provider.dart';
 
@@ -36,9 +37,11 @@ class _ManageNotificationsState extends State<ManageNotifications> {
             return Text('Error: ${snapshot.error}');
           } else {
             final settings = context.read<NetworkService>().userSettings;
-            context.read<NetworkService>().updateSettingsNotifications(false);
             mentions = settings!.notifications.mentionsNotifs;
             comments = settings.notifications.commentsNotifs;
+            print(settings.notifications.mentionsNotifs);
+            print(settings.notifications.commentsNotifs);
+            print(settings.notifications.modNotifs);
             upvotes = settings.notifications.postsUpvotesNotifs;
             replies = settings.notifications.repliesNotifs;
             followers = settings.notifications.newFollowersNotifs;
@@ -65,46 +68,71 @@ class _ManageNotificationsState extends State<ManageNotifications> {
                       SwitchButton(
                           buttonText: "Mentions of u/username",
                           buttonicon: Icons.person,
-                          onPressed: (value) {
+                          onPressed: (value) async {
                             setState(() {
                               mentions = value;
                             });
+                            bool updateMentions = await context
+                                .read<NetworkService>()
+                                .updateNotificationsSettings(
+                                    'mentionsNotifs', value);
+                            print(updateMentions);
                           },
                           switchvalue: mentions ?? true),
                       SwitchButton(
                           buttonText: "Comments on your posts",
                           buttonicon: Icons.comment,
-                          onPressed: (value) {
+                          onPressed: (value) async {
                             setState(() {
                               comments = value;
                             });
+                            bool updateComments = await context
+                                .read<NetworkService>()
+                                .updateNotificationsSettings(
+                                    'commentsNotifs', value);
+                            print(updateComments);
                           },
                           switchvalue: comments ?? true),
                       SwitchButton(
                           buttonText: "Upvotes on your posts",
                           buttonicon: Icons.arrow_upward,
-                          onPressed: (value) {
+                          onPressed: (value) async {
                             setState(() {
                               upvotes = value;
                             });
+                            bool updateUpVotes = await context
+                                .read<NetworkService>()
+                                .updateNotificationsSettings(
+                                    'postsUpvotesNotifs', value);
+                            print(updateUpVotes);
                           },
                           switchvalue: upvotes ?? true),
                       SwitchButton(
                           buttonText: "Replies to your comments",
                           buttonicon: Icons.reply,
-                          onPressed: (value) {
+                          onPressed: (value) async {
                             setState(() {
                               replies = value;
                             });
+                            bool updateReplies = await context
+                                .read<NetworkService>()
+                                .updateNotificationsSettings(
+                                    'repliesNotifs', value);
+                            print(updateReplies);
                           },
                           switchvalue: replies ?? true),
                       SwitchButton(
                           buttonText: "New followers",
                           buttonicon: Icons.person_add_alt,
-                          onPressed: (value) {
+                          onPressed: (value) async {
                             setState(() {
                               followers = value;
                             });
+                            bool updateNewFollowers = await context
+                                .read<NetworkService>()
+                                .updateNotificationsSettings(
+                                    'newFollowersNotifs', value);
+                            print(updateNewFollowers);
                           },
                           switchvalue: followers ?? true),
                       const Heading(text: "UPDATES"),
@@ -117,10 +145,15 @@ class _ManageNotificationsState extends State<ManageNotifications> {
                       SwitchButton(
                           buttonText: "Mod notifications",
                           buttonicon: Icons.shield,
-                          onPressed: (value) {
+                          onPressed: (value) async {
                             setState(() {
                               modNotifs = value;
                             });
+                            bool updateModNotifi = await context
+                                .read<NetworkService>()
+                                .updateNotificationsSettings(
+                                    'modNotifs', value);
+                            print(updateModNotifi);
                           },
                           switchvalue: modNotifs ?? true),
                     ],
