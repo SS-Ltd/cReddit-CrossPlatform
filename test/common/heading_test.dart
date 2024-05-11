25fbcfcfa82523e06c535970923bd8998c9bb592
YoussefDarwishh <youssef.darwish03@eng-st.cu.edu.eg>
2024-05-11 12:17:02 +0300

    Update heading_test.dart

--- test/common/heading_test.dart
+++ test/common/heading_test.dart
@@ -1,31 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:reddit_clone/common/Heading.dart';
+import 'package:reddit_clone/common/heading.dart';
+import 'package:reddit_clone/theme/palette.dart';
 
 void main() {
-  testWidgets('Heading renders correctly', (WidgetTester tester) async {
-    // Build our widget
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: Heading(
-            text: 'Test Heading',
-          ),
-        ),
+  testWidgets('Heading widget test', (WidgetTester tester) async {
+    // Define a value for the heading text
+    final headingText = 'Test Heading';
+
+    // Build the Heading widget
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Heading(text: headingText),
       ),
-    );
-    await tester.pump(); // Wait for any animations or async tasks to complete
+    ));
 
-    // Verify that the Container is rendered
-    expect(find.byType(Container), findsOneWidget);
+    // Verify the heading text is present
+    expect(find.text(headingText), findsOneWidget);
 
-    // Verify that the Row is rendered
-    expect(find.byType(Row), findsOneWidget);
-
-    // Verify that the Text is rendered
-    expect(find.byType(Text), findsOneWidget);
-
-    // Verify that the text is rendered correctly
-    expect(find.text('Test Heading'), findsOneWidget);
+    // Verify the heading text color
+    final Text headingWidget = tester.widget(find.text(headingText));
+    expect(headingWidget.style?.color, equals(Palette.whiteColor));
   });
 }
